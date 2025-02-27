71494b60541ddf532bdbb605833afdbe9b9c663b
ryan <120286082+diamondcougar10@users.noreply.github.com>
2025-02-27 14:26:38 -0500

    added more buttons and changed layout
    Enhanced the Tools submenu by adding a "VBS4 Setup" button, which launches the VBS4 setup executable (VBSLauncher.exe). Refined the UI layout to ensure all buttons remain properly spaced and consistently sized. Maintained the 1600x800 locked window size across all menus, ensuring the background image fully fits without stretching or cutoff issues. Retained the red Exit button in both the Main Menu and Submenus, providing a consistent navigation experience. These improvements enhance usability, improve visual consistency, and allow direct access to VBS4 setup without needing external navigation

---
+++ PythonPorjects/Autolaunch_Batchfiles/BVi_Launch.bat
@@ -0,0 +1,5 @@
+@echo off
+start "" "C:\Program Files\ARES\ARES-dev-release-v0.9.4-c1d3950\ares.manager\ares.manager.exe"
+timeout /t 40 /nobreak
+start "" "C:\Program Files\ARES\ARES-dev-release-v0.9.4-c1d3950\ares.xr\Windows\AresXR.exe"
+
