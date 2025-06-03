ff6676080a0235304efc2ce7b2c350d2dc1f37ed
diamondcougar10 <120286082+diamondcougar10@users.noreply.github.com>
2025-06-02 20:52:21 -0400

    code updates

--- PythonPorjects/Autolaunch_Batchfiles/BlueIg.bat
+++ PythonPorjects/Autolaunch_Batchfiles/BlueIg.bat
@@ -1,3 +1,3 @@
 @echo off
-"C:/BISIM/VBS4/VBS4.exe" -hmd=openxr_ctr:oculus -vbsHostExerciseID=Exercise-HAMMERKIT1-1 -splitCPU -DJobThreads=8 -DJobPool=8
+"C:\Builds\BlueIG\Blue IG 24.2 YYMEA_General\BlueIG.exe" -hmd=openxr_ctr:oculus -vbsHostExerciseID=Exercise-HAMMERKIT1-1 -splitCPU -DJobThreads=8 -DJobPool=8
 exit /b 0
--- PythonPorjects/Autolaunch_Batchfiles/VBS4_Launch.bat
+++ PythonPorjects/Autolaunch_Batchfiles/VBS4_Launch.bat
@@ -1,3 +1,3 @@
 @echo off
-"C:/BISIM/VBS4/VBS4.exe" -admin "-autoassign=admin" -forceSimul -window
+"C:\Builds\VBS4\VBS4 25.1 YYMEA_General\VBS4.exe" -admin "-autoassign=admin" -forceSimul -window
 exit /b 0
