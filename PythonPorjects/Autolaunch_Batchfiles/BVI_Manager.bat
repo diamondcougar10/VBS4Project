0b20453c2d215c27955d941daabaf3d3a835fd3e
ryan <120286082+diamondcougar10@users.noreply.github.com>
2025-06-18 11:33:24 -0400

    Use backslashes for Windows file paths

--- PythonPorjects/Autolaunch_Batchfiles/BVI_Manager.bat
+++ PythonPorjects/Autolaunch_Batchfiles/BVI_Manager.bat
@@ -1,5 +1,5 @@
 @echo off
-start "" "C:/Program Files/ARES/ARES-dev-release-v0.9.4-c1d3950/ares.manager/ares.manager.exe"
+start "" "C:\Program Files\ARES\ARES-dev-release-v0.9.4-c1d3950\ares.manager\ares.manager.exe"
 timeout /t 40 /nobreak
-start "" "C:/Program Files/ARES/ARES-dev-release-v0.9.4-c1d3950/ares.manager/ares.manager.exe"
+start "" "C:\Program Files\ARES\ARES-dev-release-v0.9.4-c1d3950\ares.manager\ares.manager.exe"
 exit /b 0
