303a508e48d9d417460fcd78a758905780db593c
diamondcougar10 <120286082+diamondcougar10@users.noreply.github.com>
2025-09-02 10:34:14 -0400

    Revert "Add preset installation and wizard autostart launcher"
    This reverts commit f81d7ad0c62e9512a2c3bd584edc691f7cf361a3.

--- PythonPorjects/photomesh/__init__.py
+++ PythonPorjects/photomesh/__init__.py
@@ -5,16 +5,12 @@ from .bootstrap import (
     launch_autostart_build,
     enforce_wizard_defaults_obj_only,
 )
-from .launch_photomesh_preset import (
-    launch_wizard_with_preset,
-    prepare_presets_and_wizard_defaults,
-)
+from .launch_photomesh_preset import launch_photomesh_with_install_preset
 
 __all__ = [
     "stage_install_preset",
     "launch_autostart_build",
     "enforce_wizard_defaults_obj_only",
-    "launch_wizard_with_preset",
-    "prepare_presets_and_wizard_defaults",
+    "launch_photomesh_with_install_preset",
 ]
 
